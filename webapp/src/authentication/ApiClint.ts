7b3fcf095b415608bd7d75ef6c78a958e570096f
concept-technology <162841525+concept-technology@users.noreply.github.com>
2025-01-14 23:56:57 +0100

    Update ApiClint.ts

--- webapp/src/authentication/ApiClint.ts
+++ webapp/src/authentication/ApiClint.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //export const SITE_DOMAIN = import.meta.env.VITE_API_URL;
-const SITE_DOMAIN = https://api.conceptsoftwares.com
+const SITE_DOMAIN = "https://api.conceptsoftwares.com"
 const apiClient = axios.create({
   baseURL: SITE_DOMAIN,
   withCredentials: true, // Allow HTTP-only cookies to be sent with requests
