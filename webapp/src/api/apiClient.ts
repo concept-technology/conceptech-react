a5924776529b7a7d175552cd0acb7496d3a18430
concept-technology <162841525+concept-technology@users.noreply.github.com>
2025-03-27 15:06:18 +0100

    added api variable

--- webapp/src/api/apiClient.ts
+++ webapp/src/api/apiClient.ts
@@ -1,5 +1,5 @@
 import Cookies from "js-cookie"
-export const SITE_DOMAIN = 'http://localhost:8000'
+export const SITE_DOMAIN = import.meta.env.VITE_API_URL
 export const RECAPTCHA_SITE_KEY  = import.meta.env.VITE_RECAPTCHA_SITE_KEY
 export const token = Cookies.get('accessToken')
 export const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID
